82be89832686d586b50d0549276753b9e14ae7e2
John Sirois <john.sirois@gmail.com>
2022-12-28 14:17:15 -0800

    Set up / prove out dual target "ownership".

--- experiments/a.py
+++ experiments/a.py
@@ -1,6 +1,19 @@
+import io
+import os
+import sys
+
+def app():
+  print("Hello World!")
+
 def a1_main():
-  print("Running a1_main")
-  assert True
+  orig_stdout = sys.stdout
+  test_output = io.StringIO()
+  sys.stdout = test_output
+  try:
+    app()
+  finally:
+    sys.stdout = orig_stdout
+  assert f"Hello World!{os.linesep}" == test_output.getvalue()
 
 def a2_main():
   print("Running a2_main")
