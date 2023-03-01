0ee783f9efdf510c6b06c0bbb456bdf67b9b9d35
Bruce Eckel <BruceTEckel@gmail.com>
2023-03-01 14:38:20 -0700

    more

--- experiments/rust_objects2/main.rs
+++ experiments/rust_objects2/main.rs
@@ -1,56 +1,45 @@
-//: RethinkingObjects/experiments/rust_objects1/main.rs
+//: RethinkingObjects/experiments/rust_objects2/main.rs
 
-struct Base {
+struct A {
   description: String,
+}
+
+struct B {
   count: i32,
+}
+
+struct C {
+  id: i8,
+}
+
+// Compose by creating subobjects in new struct:
+struct All {
+  a: A,
+  b: B,
+  c: C,
 }
 
 trait Info {
   fn description(&self) -> &String;
   fn count(&self) -> i32;
+  fn id(&self) -> i8;
 }
 
-impl Info for Base {
-  fn description(&self) -> &String { &self.description }
-  fn count(&self) -> i32 { self.count }
+impl Info for All {
+  fn description(&self) -> &String { &self.a.description }
+  fn count(&self) -> i32 { self.b.count }
+  fn id(&self) -> i8 { self.c.id }
 }
-
-trait InfoB {
-  fn summary_b(&self);
-}
-
-impl InfoB for Base {  // Notice Base is a struct
-  fn summary_b(&self) {
-    println!("summary_b(): {}: {}", self.description, self.count);
-  }
-}
-
-trait InfoC {
-  fn summary_c(&self);
-}
-
-impl<T: Info> InfoC for T {  // T must be a trait, not a struct (!)
-  fn summary_c(&self) {
-    println!("summary_c(): {}: {}", self.description(), self.count());
-  }
-}  // Notice: No explicit InfoC for Base!
-
-// Or you could do it like this -- but not at the same time (conflict):
-// impl<T: InfoB> InfoC for T {
-//   fn summary_c(&self) {
-//     self.summary_b();
-//   }
-// }
-
 
 fn main() {
-  let b = Base {
-    description: String::from("hello world"),
-    count: 11,
+  let a = All {
+    a: A { description: String::from("hello world") },
+    b: B { count: 11 },
+    c: C { id: 3 },
   };
-  println!("b.description(): {}", b.description());
-  b.summary_b();
-  b.summary_c();
+  println!("a.description(): {}", a.description());
+  println!("a.count(): {}", a.count());
+  println!("a.id(): {}", a.id());
 }
 
 
