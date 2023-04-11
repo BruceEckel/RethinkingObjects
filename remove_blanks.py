ea36ef850e18d504bcff6782b0d6c0a0404ca5e3
Bruce Eckel <BruceTEckel@gmail.com>
2023-04-11 10:53:38 -0600

    Added annotations for clarity

--- remove_blanks.py
+++ remove_blanks.py
@@ -1,22 +1,25 @@
 """
-Skips consecutive blank lines.
+Replaces consecutive blank lines with a single one.
 Preserves/ensures a blank line at the end of the file.
 Only writes file if it is unchanged, to prevent spurious
-Github commits from touching the file.
+Github commits by touching the file.
 """
+from collections.abc import Iterator
 from pathlib import Path
 
-def filter_lines(lines):
-    prev_line = None
+def filter_lines(lines: list[str]) -> Iterator[str]:
+    prev_line: str = None
     for line in lines:
         if line.strip() or prev_line.strip():
             yield line
         prev_line = line
 
-current_dir = Path('.')
-for file_path in current_dir.glob('*.py'):
-    lines = file_path.read_text().strip().splitlines()
-    new_lines = list(filter_lines(lines))
-    new_text = '\n'.join(new_lines) + '\n'
+current_dir: Path = Path(".")
+for file_path in current_dir.glob("*.py"):
+    lines: list[str] = (
+        file_path.read_text().strip().splitlines()
+    )
+    new_lines: list[str] = list(filter_lines(lines))
+    new_text: str = "\n".join(new_lines) + "\n"
     if new_text != file_path.read_text():
         file_path.write_text(new_text)
