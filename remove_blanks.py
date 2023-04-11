da80fce6f1fbfb86b549e6f66ddbf007f0d86ffb
Bruce Eckel <BruceTEckel@gmail.com>
2023-04-11 10:37:01 -0600

    Update remove_blanks.py
    Doesn't touch files unnecessarily

--- remove_blanks.py
+++ remove_blanks.py
@@ -1,17 +1,22 @@
 """
 Skips consecutive blank lines.
 Preserves/ensures a blank line at the end of the file.
+Only writes file if it is unchanged, to prevent spurious
+Github commits from touching the file.
 """
 from pathlib import Path
 
+def filter_lines(lines):
+    prev_line = None
+    for line in lines:
+        if line.strip() or prev_line.strip():
+            yield line
+        prev_line = line
+
 current_dir = Path('.')
 for file_path in current_dir.glob('*.py'):
-    lines = file_path.read_text().splitlines()
-    new_lines = []
-    for i, line in enumerate(lines):
-        if i < len(lines) - 2 and not line.strip() and not lines[i + 1].strip():
-            continue
-        new_lines.append(line)
-    if lines[-1].strip() and (len(lines) == 1 or lines[-2].strip()):
-        new_lines.append(lines[-1])
-    file_path.write_text('\n'.join(new_lines))
+    lines = file_path.read_text().strip().splitlines()
+    new_lines = list(filter_lines(lines))
+    new_text = '\n'.join(new_lines) + '\n'
+    if new_text != file_path.read_text():
+        file_path.write_text(new_text)
