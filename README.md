18e6c37d5d7ffea7317d21ebb56822c88d600760
Bruce Eckel <BruceTEckel@gmail.com>
2023-04-11 17:59:49 -0600

    User instructions

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # Rethinking Objects
 
-Code Examples From the presentation.
+Code examples from the presentation **Rethinking Objects** presented at
+PyCon 2023 in Salt Lake City.
 
+You can find the (Google) slides for the presentation
+[here](https://docs.google.com/presentation/d/1U0Mw4Aaz6mf5KpS-mZqKlK1H2_8JxXO6VF4axk9OPXY/edit?usp=sharing).
 
+After cloning this repo, use `pip` or `pipx` to install the `hatch`
+build system. Move to the root directory of the cloned repo. Then
+run:
+```
+hatch shell
+```
+Which will set up and configure a virtual environment. Now you can run:
+```
+pytest
+ruff check .
+mypy .
+```
+These verify the code.
--- untouch.py
+++ untouch.py
@@ -1,19 +1,27 @@
-from typing import List
-import git
+from typing import List, Optional
 from pathlib import Path
+import git
 
+def discard_changes_if_identical_to_previous_commit(file_path: Path) -> Optional[str]:
+    try:
+        repo = git.Repo(".", search_parent_directories=True)
+    except git.InvalidGitRepositoryError:
+        print("Error: Not inside a Git repository")
+        return None
 
-def discard_changes_if_identical_to_previous_commit(file_path: Path) -> None:
-    repo = git.Repo(file_path, search_parent_directories=True)
-    current_file_content = file_path.read_text().strip()
-    previous_file_content = repo.git.show(f"{repo.head.commit.hexsha}:{file_path}")
-    previous_file_content = previous_file_content.strip()
-    if current_file_content == previous_file_content:
-        print(f"{file_path} has not been modified since the last commit.")
+    if file_path.is_absolute():
+        file_path = file_path.relative_to(repo.working_dir)
+
+    # check whether the file has been modified
+    if repo.is_dirty(file_path):
+        # discard changes
+        try:
+            repo.git.checkout(file_path)
+            return f"Discarded changes for {file_path}"
+        except git.GitCommandError as e:
+            return f"Error discarding changes for {file_path}: {e.stderr.strip()}"
     else:
-        print(f"{file_path} has been modified since the last commit. Discarding changes...")
-        repo.git.checkout("--", str(file_path))
-
+        return f"{file_path} has not been modified"
 
 if __name__ == '__main__':
     python_files: List[Path] = list(Path(".").glob("*.py"))
