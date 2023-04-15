0d537d9480f6edeea8209dcfb2d918eeeda516fd
Bruce Eckel <BruceTEckel@gmail.com>
2023-04-15 11:47:05 -0600

    Cleanup

--- remove_blanks.py
+++ remove_blanks.py
@@ -1,11 +1,14 @@
 """
+For presentations only, where multiple blank lines use
+up valuable screen real estate. Cleans up after running Black.
+
 Replaces consecutive blank lines with a single one.
-Preserves/ensures a blank line at the end of the file.
+Ensures a single blank line at the end of the file.
+Strips blanks at the beginning of the file.
 Only writes file if it is unchanged.
 """
 from collections.abc import Iterator
 from pathlib import Path
-from typing import Optional
 
 def _clean_multiple_blanks(lines: list[str]) -> Iterator[str]:
     prev_line: str = ""
@@ -14,23 +17,18 @@ def _clean_multiple_blanks(lines: list[str]) -> Iterator[str]:
             yield line
         prev_line = line
 
-def deduplicate_blanks(text: str) -> str | None:
-    lines: list[str] = text.strip().splitlines()
+def deduplicate_blanks(original: str) -> str | None:
+    lines: list[str] = original.strip().splitlines()
     new_lines: list[str] = list(_clean_multiple_blanks(lines))
     new_text: str = "\n".join(new_lines) + "\n"
-    if new_text != text:
+    if new_text != original:
         return new_text
     return None
 
-if __name__ == '__main__':        
+if __name__ == "__main__":
     for fpath in Path(".").glob("*.py"):
         match deduplicate_blanks(fpath.read_text()):
-            case None: continue  # No changes
-            case str(text): fpath.write_text(new_text)
-        # lines: list[str] = (
-        #     fpath.read_text().strip().splitlines()
-        # )
-        # new_lines: list[str] = list(deduplicate_blanks(lines))
-        # new_text: str = "\n".join(new_lines) + "\n"
-        # if new_text != fpath.read_text():
-        #     fpath.write_text(new_text)
+            case None:
+                continue  # No changes
+            case str(text):
+                fpath.write_text(text)
