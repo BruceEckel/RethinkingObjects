40372a692ff62da608968a11edba1970f0b4e471
Bruce Eckel <BruceTEckel@gmail.com>
2023-04-17 11:39:31 -0600

    cleanup
    Iterator->Generator. Ruff likes collections.abc over types

--- remove_blanks.py
+++ remove_blanks.py
@@ -7,10 +7,10 @@ Ensures a single blank line at the end of the file.
 Strips blanks at the beginning of the file.
 Only writes file if it is unchanged.
 """
-from collections.abc import Iterator
+from collections.abc import Generator
 from pathlib import Path
 
-def _clean_multiple_blanks(lines: list[str]) -> Iterator[str]:
+def _clean_multiple_blanks(lines: list[str]) -> Generator[str, None, None]:
     prev_line: str = ""
     for line in lines:
         if line.strip() or prev_line.strip():
